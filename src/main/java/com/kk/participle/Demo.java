7f2c5fe8fce1a09ab99c4fa0771e3cf2ae84493d
kk_global <kk_global@126.com>
2019-06-25 23:51:11 +0800

    java-application learn...

--- src/main/java/com/kk/participle/Demo.java
+++ src/main/java/com/kk/participle/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
     public static void demo(){
         SegTag tag=new SegTag(1); //分词路径
         String str="今天天气不错,我想出去玩,你呢?";
-        //TODO:此处有BUG,暂未在网上找到对应源码!
+        //TODO:此处有BUG,会抛出空指针异常,暂未在网上找到对应源码!
         SegResult result = tag.split(str);
 
         System.out.println(result.getFinalResult());
